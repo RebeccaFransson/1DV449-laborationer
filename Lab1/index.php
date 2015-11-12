eb6adb743bc8766dc516d0b396d858d5ce2c9a51
rf222cz <rf222cz@student.lnu.se>
2015-11-12 11:56:02 +0100

    Yey :gem: i'am scraping! :zap:

--- Lab1/index.php
+++ Lab1/index.php
@@ -1,1 +1,18 @@
 <?php
+
+
+$data = file_get_contents('http://localhost:8080/');
+
+$dom = new DOMDocument();
+if($dom->loadHTML($data)){
+  $xPath = new DOMXPath($dom);
+  $list = $xPath->query('//a');
+  //var_dump($list);
+
+  foreach ($list as $item) {
+    echo $item->nodeValue ."  -->  ". $item->getAttribute("href") ."<br><br>";
+  }
+}else{
+  echo "Fel av instläsning av html";
+}
+//var_dump($dom);
