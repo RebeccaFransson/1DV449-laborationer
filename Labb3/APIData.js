bd61e33789dc3c451da64202057648ebca6e93ec
rf222cz <rf222cz@student.lnu.se>
2015-12-14 19:21:48 +0100

    started on ordering categorys

--- Labb3/APIData.js
+++ Labb3/APIData.js
@@ -9,26 +9,51 @@ var APIData = {
             complete: function(data){
                 var parsed = JSON.parse(JSON.stringify(data));
                 var xml = parsed.responseText;
-                APIData.response(xml);
+                var catArray = Array();
+                var allArray = Array();
+                var cat;
+                document.getElementById("cat").addEventListener("click", function(e){
+                  cat = e.target.id;
+                  APIData.response(xml, cat);
+
+                });
+                /*$(xml).find('message').each(function(){
+                  if(cat){
+                    if($(this).children('category').text() == e.target.id){
+                      allArray.push($(this));
+                    }else if($(this)){
+                      allArray.push($(this))
+                    }
+                  }else{
+                    allArray.push($(this))
+                  }
+                });*/
+
+                APIData.response(xml, cat);
               }
        });
   },
 
-  response: function(data){
+  response: function(data, cat){
+    document.querySelector('#list').innerHTML = '';
     var allData = Array();
+
     $(data).find('message').each(function(){
-            var message = $(this);
-            var title = message.children('title').text();
-            var subCategory = message.children('subcategory');
-            var description = message.children('description').text();
-            var latitude = parseInt(message.children('latitude').text());
-            var longitude = parseInt(message.children('longitude').text());
-            var date = message.children('createddate').text().slice(0, 10);
-            
+      if($(this).children('category').text() == cat){
+        console.log('cat vald'+cat);
+      }
+      //TODO: skriv ut de valda kategorierna
+            var title = $(this).children('title').text();
+            var subCategory = $(this).children('subcategory');
+            var description = $(this).children('description').text();
+            var latitude = parseInt($(this).children('latitude').text());
+            var longitude = parseInt($(this).children('longitude').text());
+            var date = $(this).children('createddate').text().slice(0, 10);
+
             if(!description){
               description = 'Ingen beskrivning';
             }
-            var html = '<div class="message"><h3>' + title + '</h3></div>';
+            var html = '<div class="message"><h3>' + title + '</h3>'+date+'</div>';
             //var tit = document.createElement('h3').appendChild(document.createTextNode(title.text()));
             //var cat = document.createElement('p').appendChild(document.createTextNode(subCategory.text()));
             //var des = document.createElement('p').appendChild(document.createTextNode(description.text()));
